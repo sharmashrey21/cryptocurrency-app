04502ba8aaac6611c7dfb8ae7327d8db8d2cf0d2
Shrey Sharma <shrey.sharma@edu.uwaterloo.ca>
2018-10-02 17:29:09 -0400

    Label with date functionality is working but the price is incorrect

--- src/PriceChecker/PriceChecker.js
+++ src/PriceChecker/PriceChecker.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import '../PriceChecker/PriceChecker.css';
 
 class PriceChecker extends Component {
+  state = {
+    value: '',
+    data: [
+      {
+        id: 'bitcoin',
+        price_usd: '1',
+        last_updated: '0'
+      }
+    ]
+  };
+
+  fetchData() {
+    axios
+      .get('https://api.bitcoincharts.com/v1/trades.csv?symbol=bitstampUSD')
+      .then(response => {
+        console.log('Response: ', response);
+        var cryptoData = ['bitcoin'];
+        var date = this.state.value;
+        var result = response.data.filter(currency => cryptoData.includes(date));
+        this.setState({ data: result });
+      })
+      .catch(err => console.log(err));
+  }
+
+  convertDate(value) {
+    const d = new Date(value);
+    var date = d.getTime();
+    this.setState({ value: date });
+    return date;
+  }
+
+  handleChange = async event => {
+    this.convertDate(this.state.value);
+    this.fetchData();
+    console.log('state: ', this.state);
+  };
+
   render() {
     return (
-      <div>
-        <input type="date" />
+      <div className="BitcoinPrice">
+        <p>Check Price of Bitcoin on a particular day!</p>
+        <input type="date" id="bitcoin-date" onChange={this.handleChange} /> <br />
+        <label htmlFor="bitcoin-date">{this.state.value}</label>
       </div>
     );
   }
