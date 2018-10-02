ecc94d5054f792b762d0e99e82dac1c7d612d1e2
Shrey Sharma <shrey.sharma@edu.uwaterloo.ca>
2018-10-02 11:20:08 -0400

    added input type date for PriceChecker

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Tickers from '../Ticker/Tickers';
+import PriceChecker from '../PriceChecker/PriceChecker';
 import '../App/App.css';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
           <h2>Your Cryptocurrency Haven</h2>
         </div>
         <Tickers />
+        <PriceChecker />
       </div>
     );
   }
---
+++ src/PriceChecker/PriceChecker.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../PriceChecker/PriceChecker.css';
+
+class PriceChecker extends Component {
+  render() {
+    return (
+      <div>
+        <input type="date" />
+      </div>
+    );
+  }
+}
+
+export default PriceChecker;
