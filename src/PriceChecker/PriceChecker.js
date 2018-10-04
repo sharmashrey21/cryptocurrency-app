8d57862ebf3ca5dbb0437092e2b738254bac722d
Shrey Sharma <shrey.sharma@edu.uwaterloo.ca>
2018-10-04 17:35:48 -0400

    getting better with the date selection

--- src/PriceChecker/PriceChecker.js
+++ src/PriceChecker/PriceChecker.js
@@ -5,40 +5,43 @@ import '../PriceChecker/PriceChecker.css';
 
 class PriceChecker extends Component {
   state = {
-    value: '',
-    data: [
-      {
-        id: 'bitcoin',
-        price_usd: '1',
-        last_updated: '0'
-      }
-    ]
+    price_usd: '',
+    date: '',
+    datePlusOne: ''
   };
 
   fetchData() {
     axios
-      .get('https://api.bitcoincharts.com/v1/trades.csv?symbol=bitstampUSD')
+      .get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05')
       .then(response => {
         console.log('Response: ', response);
-        var cryptoData = ['bitcoin'];
-        var date = this.state.value;
-        var result = response.data.filter(currency => cryptoData.includes(date));
-        this.setState({ data: result });
+        var result = response.data.bpi;
+        console.log('Result: ', result);
+        console.log('state: ', this.state);
       })
       .catch(err => console.log(err));
   }
 
-  convertDate(value) {
-    const d = new Date(value);
-    var date = d.getTime();
-    this.setState({ value: date });
-    return date;
-  }
+  // convertDate(value) {
+  //   console.log('value: ', value);
+  //   const d = new Date(value);
+  //   var date = d.getTime();
+  //   this.setState({ value: date });
+  //   console.log('state: ', this.state);
+  //   console.log('date: ', date);
+  //   return date;
+  // }
 
   handleChange = async event => {
-    this.convertDate(this.state.value);
-    this.fetchData();
-    console.log('state: ', this.state);
+    var dateSelected = event.target.value;
+    console.log('dateSelected: ', dateSelected);
+    this.setState({ date: dateSelected });
+    var numberDate = dateSelected.length - 2;
+    var dateTomorrow = Number.parseInt(dateSelected.substring(numberDate, dateSelected.length));
+    dateTomorrow++;
+    console.log('dateTomorrow: ', dateTomorrow);
+    // this.setState({ datePlusOne: tomorrowString });
+    await this.fetchData();
   };
 
   render() {
@@ -46,7 +49,7 @@ class PriceChecker extends Component {
       <div className="BitcoinPrice">
         <p>Check Price of Bitcoin on a particular day!</p>
         <input type="date" id="bitcoin-date" onChange={this.handleChange} /> <br />
-        <label htmlFor="bitcoin-date">{this.state.value}</label>
+        <label htmlFor="bitcoin-date">{this.state.price_usd}</label>
       </div>
     );
   }
